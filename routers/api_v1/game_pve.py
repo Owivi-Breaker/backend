3cd54dc6664a089bea4b017d78926268342bcd3e
MelonSoul <1375911011@qq.com>
2022-01-29 12:16:26 +0800

    docs(api_v1/game_pve): 增添阵容和战术比重例子

--- routers/api_v1/game_pve.py
+++ routers/api_v1/game_pve.py
@@ -1,9 +1,8 @@
-import json
 from typing import List, Dict, Union, Optional
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-import random
+from pydantic import BaseModel, Field
 
 import schemas
 import models
@@ -12,15 +11,31 @@ from utils import logger, utils
 import utils
 from core.db import engine, get_db
 from modules import game_app, generate_app, computed_data_app
-import game_configs
+
+
+class TacticWeight(BaseModel):
+    """
+    战术比重
+    """
+    wing_cross: int
+    under_cutting: int
+    pull_back: int
+    middle_attack: int
+    counter_attack: int
+
+
+lineup_example = {1: "ST", 18: "LW", 23: 'RW', 42: 'CM', 128: 'CAM', 256: 'CDM',
+                  122: 'CB', 119: 'CB', 67: 'LB', 243: 'RB', 1231: 'GK'
+                  }
 
 router = APIRouter()
 
 
-def commit_lineup_n_tactic_weight(db: Session,
-                                  save_model: models.Save,
-                                  lineup: Optional[Dict[int, str]] = None,
-                                  tactic_weight: Optional[Dict[str, int]] = None):
+def commit_lineup_n_tactic_weight(
+        db: Session,
+        save_model: models.Save,
+        lineup: Optional[Dict[int, str]] = None,
+        tactic_weight: TacticWeight = None):
     """
     保存玩家阵容和战术比重
     """
@@ -39,20 +54,21 @@ def commit_lineup_n_tactic_weight(db: Session,
     else:
         # 保存战术比重
         coach_model = crud.get_coach_by_club(db=db, club_id=save_model.player_club_id)
-        coach_model.wing_cross = tactic_weight['wing_cross']
-        coach_model.under_cutting = tactic_weight['under_cutting']
-        coach_model.pull_back = tactic_weight['pull_back']
-        coach_model.middle_attack = tactic_weight['middle_attack']
-        coach_model.counter_attack = tactic_weight['counter_attack']
+        coach_model.wing_cross = tactic_weight.wing_cross
+        coach_model.under_cutting = tactic_weight.under_cutting
+        coach_model.pull_back = tactic_weight.pull_back
+        coach_model.middle_attack = tactic_weight.middle_attack
+        coach_model.counter_attack = tactic_weight.counter_attack
         db.commit()
 
 
 @router.post('/skip')
 def skip_game_pve(
-        lineup: Optional[Dict[int, str]] = None,
-        tactic_weight: Optional[Dict[str, int]] = None,
-        db: Session = Depends(get_db),
-        save_model: models.Save = Depends(utils.get_current_save)):
+        tactic_weight: TacticWeight = None,
+        lineup: Optional[Dict[int, str]] = Body(
+            None, example=lineup_example),
+        save_model: models.Save = Depends(utils.get_current_save),
+        db: Session = Depends(get_db)):
     """
     保存玩家阵容和战术比重 并且跳过pve比赛
     """
@@ -73,12 +89,15 @@ def skip_game_pve(
 
 @router.post('/start')
 def start_game_pve(
-        lineup: Optional[Dict[int, str]] = None,
-        tactic_weight: Optional[Dict[str, int]] = None,
+        tactic_weight: TacticWeight = None,
+        lineup: Optional[Dict[int, str]] = Body(
+            None, example=lineup_example),
         db: Session = Depends(get_db),
         save_model: models.Save = Depends(utils.get_current_save)):
     """
     保存玩家阵容和战术比重 并且准备开始pve比赛
+    :param tactic_weight:战术权重
+    :param lineup: 阵容
     :return: 进攻方club_id
     """
     commit_lineup_n_tactic_weight(lineup=lineup, tactic_weight=tactic_weight, db=db, save_model=save_model)
@@ -98,7 +117,6 @@ def next_turn(tactic: str = None,
     下一回合
     :param tactic: 玩家球队的进攻战术名
     """
-
     # 开始回合
     game_pve = game_app.GamePvE(save_id=save_model.id, db=db, player_tactic=tactic)
     is_finished = game_pve.start_one_turn()
